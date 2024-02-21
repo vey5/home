4bd890702d1730150623f0cf35c0f9dd10d96cf9
vey5 <vlad.9505@mail.ru>
2024-02-21 21:37:58 +0300

    добавил баннер для хедера и секции контента под ним

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,11 +2,13 @@ import { FC, PropsWithChildren } from 'react'
 import styles from './styles.module.scss'
 import { Header } from '../Header'
 import { Footer } from '../Footer'
+import { Banner } from '../Content/Banner'
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className={styles.layout}>
       <Header />
+      <Banner />
       <div className={styles.content}>{children}</div>
       <div className={styles.footContainer}>
         <div className={styles.footer}>
