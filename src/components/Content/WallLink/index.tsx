cff68cfce032845155399a39847f20fbbc4b5f58
vey5 <vlad.9505@mail.ru>
2024-03-15 16:07:30 +0300

    добавил адаптив в header, banner, wallSection, переделал wallLink

--- src/components/Content/WallLink/index.tsx
+++ src/components/Content/WallLink/index.tsx
@@ -1,34 +1,27 @@
 import styles from './styles.module.scss'
 import { FC } from 'react'
 import CheckIcon from '@mui/icons-material/Check'
+import classNames from 'classnames'
 
 const WallLink: FC = () => {
   return (
     <div className={styles.wallLink}>
-      <span className={styles.cardLink1}>
-        <h2 className={styles.cardTitle}>Looking for</h2>
-        <div className={styles.check}>
-          <CheckIcon />
-        </div>
-      </span>
-      <span className={styles.cardLink2}>
-        <h2 className={styles.cardTitle}>Location</h2>
-        <div className={styles.check2}>
-          <CheckIcon />
-        </div>
-      </span>
-      <span className={styles.cardLink3}>
-        <h2 className={styles.cardTitle}>Property Type</h2>
-        <div className={styles.check3}>
-          <CheckIcon />
-        </div>
-      </span>
-      <span className={styles.cardLink4}>
-        <h2 className={styles.cardTitle}>Price</h2>
-        <div className={styles.check4}>
-          <CheckIcon />
-        </div>
-      </span>
+      <div className={styles.card}>
+        <span className={styles.span}>Looking for</span>
+        <CheckIcon />
+      </div>
+      <div className={styles.card2}>
+        <span className={styles.span}>Location</span>
+        <CheckIcon />
+      </div>
+      <div className={styles.card2}>
+        <span className={styles.span}>Property Type</span>
+        <CheckIcon />
+      </div>
+      <div className={classNames(styles.card, styles.cardSecondary)}>
+        <span className={styles.span}>Price</span>
+        <CheckIcon />
+      </div>
     </div>
   )
 }
