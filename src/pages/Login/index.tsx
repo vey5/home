3abccd0ad5c0915df809feefbc06125ad67c96f3
Сергей Абрашкин <abras@MacBook-Pro-Sergej.local>
2024-06-21 00:24:11 +0300

    envs upd

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -26,7 +26,7 @@ const Login: FC = () => {
   }
 
   const submit = (_data: any) => {
-    fetch(`${process.env}/login`, {
+    fetch(`${process.env.REACT_APP_API_URL}/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
